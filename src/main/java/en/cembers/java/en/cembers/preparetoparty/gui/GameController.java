c80fd2ea2a220bcd2cb99edf18c5f4a3c824e769
Christina <christina.embers@gmail.com>
2025-06-15 14:58:44 +0200

    refactored ActionBuilder and added connection from gamemanager to ActionBuilder

--- src/main/java/en/cembers/java/en/cembers/preparetoparty/gui/GameController.java
+++ src/main/java/en/cembers/java/en/cembers/preparetoparty/gui/GameController.java
@@ -1,16 +1,29 @@
 package en.cembers.preparetoparty.gui;
 
+import en.cembers.preparetoparty.logic.ActionBuilder;
+import en.cembers.preparetoparty.model.Action;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.ArrayList;
 
 /**
  * This class is responsible for UI interactions
  */
 public class GameController implements Initializable {
     public static final int CURRENT_OPTION_SET=0;
+    private Map<String, ArrayList<Action>> gameOptions = new HashMap<>();
 
+    private void fillMap()
+    {
+gameOptions.put("Food", ActionBuilder.getFoodActions());
+gameOptions.put("Deco", ActionBuilder.getDecoActions());
+gameOptions.put("Activity", ActionBuilder.getActivityActions());
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //todo initialisiere die map
--- src/main/java/en/cembers/java/en/cembers/preparetoparty/logic/ActionBuilder.java
+++ src/main/java/en/cembers/java/en/cembers/preparetoparty/logic/ActionBuilder.java
@@ -10,119 +10,69 @@ import java.util.ArrayList;
  */
 public class ActionBuilder {
 
-public static ArrayList<Action> foodActions= new ArrayList<Action>();
-public static ArrayList<Action> decoActions= new ArrayList<Action>();
-public static ArrayList<Action> activityActions= new ArrayList<Action>();
+    private static final ArrayList<Action> FOOD_ACTIONS = new ArrayList<Action>();
+    private static final ArrayList<Action> DECO_ACTIONS = new ArrayList<Action>();
+    private static final ArrayList<Action> ACTIVITY_ACTIONS = new ArrayList<Action>();
 
+    static {
+        // Food Actions
+        FOOD_ACTIONS.add(new Action(
+                "Simple Sandwiches",
+                "You quickly prepare some simple sandwiches. It’s not fancy, but it works.",
+                30, 20, 6, 5));
 
-    //region Food Actions
-   private Action food1 = new Action(
-            "Simple Sandwiches",
-            "You quickly prepare some simple sandwiches. It’s not fancy, but it works.",
-            30,
-            20,
-            6,
-            5);
+        FOOD_ACTIONS.add(new Action(
+                "Catering Service",
+                "You order professional catering. Convenient, but pricey.",
+                20, 5, 9, 50));
 
-    private Action food2 = new Action(
-            "Catering Service",
-            "You order professional catering. Convenient, but pricey.",
-            20,
-            5,
-            9,
-            50);
+        FOOD_ACTIONS.add(new Action(
+                "Home Cooked Meal",
+                "You decide to make a home cooked meal, delicious, but a lot of work.",
+                120, 40, 13, 20));
 
-    private Action food3 = new Action(
-            "Home Cooked Meal",
-            "You decide to make a home cooked meal, delicious, but a lot of work.",
-            120,
-            40,
-            13,
-            20);
-    //endregion
+        // Decoration Actions
+        DECO_ACTIONS.add(new Action(
+                "Minimal Decorations",
+                "You put up a few decorations from last year.",
+                15, 5, 5, 2));
 
-    //region Decoration
-    private Action decor1 = new Action(
-            "Minimal Decorations",
-            "You put up a few decorations from last year.",
-            15,
-            5,
-            5,
-            2);
+        DECO_ACTIONS.add(new Action(
+                "Colorful Banners",
+                "You hang colorful banners and streamers across the room.",
+                45, 15, 8, 8));
 
-    private Action decor2 = new Action(
-            "Colorful Banners",
-            "You hang colorful banners and streamers across the room.",
-            45,
-            15,
-            8,
-            8);
+        DECO_ACTIONS.add(new Action(
+                "Themed Setup",
+                "You create a fully themed setup with lights, tablecloths and more.",
+                90, 30, 20, 30));
 
-    private Action decor3 = new Action(
-            "Themed Setup",
-            "You create a fully themed setup with lights, tablecloths and more.",
-            90,
-            30,
-            20,
-            30);
+        // Activity Actions
+        ACTIVITY_ACTIONS.add(new Action(
+                "Lounge Music",
+                "You play some relaxed background music. Low effort, decent effect.",
+                10, 5, 4, 0));
 
-    //endregion
+        ACTIVITY_ACTIONS.add(new Action(
+                "Board Games",
+                "You set up a variety of board games for guests to enjoy.",
+                20, 20, 9, 20));
 
-    //region Activities
-    private Action activity1 = new Action(
-            "Lounge Music",
-            "You play some relaxed background music. Low effort, decent effect.",
-            10,
-            5,
-            4,
-            0);
-
-    private Action activity2 = new Action(
-            "Board Games",
-            "You set up a variety of board games for guests to enjoy.",
-            20,
-            20,
-            9,
-            20);
-
-    private Action activity3 = new Action(
-            "Karaoke Night",
-            "You organize a karaoke night. It’s loud, fun, and unforgettable.",
-            120,
-            40,
-            14,
-            30);
-
-    //endregion
-
-
-    private void fillArrayLists()
-    {
-        foodActions.add(food1);
-        foodActions.add(food2);
-        foodActions.add(food3);
-
-        decoActions.add(decor1);
-        decoActions.add(decor2);
-        decoActions.add(decor3);
-
-        activityActions.add(activity1);
-        activityActions.add(activity2);
-        activityActions.add(activity3);
+        ACTIVITY_ACTIONS.add(new Action(
+                "Karaoke Night",
+                "You organize a karaoke night. It’s loud, fun, and unforgettable.",
+                120, 40, 14, 30));
     }
 
-    public ArrayList<Action> getFoodActions()
-    {
-        //todo refactoring nötig, wenn dies mehr als einmal aufgerufen wird, z.b. durch replay button
-        //oder wenn food nicht als erstes aufgerufen wird
-        fillArrayLists();
-        return foodActions;}
-
-    public ArrayList<Action> getDecoActions() {
-        return decoActions;
+    public static ArrayList<Action> getFoodActions() {
+        return FOOD_ACTIONS;
     }
 
-    public  ArrayList<Action> getActivityActions() {
-        return activityActions;
+    public static ArrayList<Action> getDecoActions() {
+        return DECO_ACTIONS;
+    }
+
+    public static ArrayList<Action> getActivityActions() {
+        return ACTIVITY_ACTIONS;
     }
 }
